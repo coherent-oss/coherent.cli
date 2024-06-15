920e6b10001dc2e6d0d1cb189ebf67c5aca0c7cd
Jason R. Coombs <jaraco@jaraco.com>
2024-06-15 09:43:50 -0400

    Implement the first command, 'install'.
    Ref coherent-oss/system#8

---
+++ __init__.py
@@ -0,0 +1,4 @@
+__requires__ = [
+	'coherent.build',
+	'typer',
+]
--- __main__.py
+++ __main__.py
@@ -1,10 +1,19 @@
+import pathlib
+import subprocess
 import sys
 
+import typer
+from coherent.build import bootstrap
 
-def main():
-    command = sys.argv[1]
-    print("command is", command)
+
+app = typer.Typer()
+
+
+@app.command()
+def install(target: pathlib.Path):
+    with bootstrap.write_pyproject(target):
+        subprocess.run([sys.executable], '-m', 'pip', 'install', target)
 
 
 if __name__ == '__main__':
-    main()
+    app()
